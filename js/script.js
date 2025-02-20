653b9bd1bf4ba3c038bd4ab61289b72c8a5a6a13
Vitalii <saifutdinov_vitalii@mail.ru>
2025-02-20 16:52:02 +0500

    fdf

--- js/script.js
+++ js/script.js
@@ -20,3 +20,58 @@ new Splide( '#slider1',
         }
     }
  ).mount( window.splide.Extensions);
+const FAQBox = document.querySelectorAll('.FAQ__box'),
+      FAQItem = document.querySelectorAll('.FAQ__item');
+function hiddenFAQ() {
+  FAQBox.forEach(item => {
+    item.classList.remove('active')
+  })
+}   
+hiddenFAQ;
+function AddFAQ(i) {
+  FAQBox[i].classList.toggle('active')
+};
+FAQItem.forEach((item, index)=> {
+  item.addEventListener('click', ()=> {
+    AddFAQ(index)
+  })
+});
+//menu
+const hamburger = document.querySelector('.hamburger'), 
+      menu = document.querySelector('.menu');
+hamburger.addEventListener('click', ()=> {
+  menu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+//popup
+const headerMore = document.querySelector('.header__more');
+const header = document.querySelector('.header');
+const popupMenu = document.querySelector('.popupMenu');
+
+headerMore.addEventListener('mouseenter', () => {
+  header.classList.add('active');
+  popupMenu.classList.add('active');
+});
+
+headerMore.addEventListener('mouseleave', () => {
+  if (!popupMenu.classList.contains('active')) {
+    closePopup();
+  }
+});
+
+popupMenu.addEventListener('mouseenter', () => {
+  header.classList.add('active');
+  popupMenu.classList.add('active');
+});
+
+popupMenu.addEventListener('mouseleave', () => {
+  closePopup();
+});
+
+function closePopup() {
+  header.classList.remove('active');
+  popupMenu.classList.remove('active');
+}
+
+  
+       
